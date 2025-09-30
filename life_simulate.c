562297796ae96adc0088844a8125dfc188aba890
tndreka <ndrekalanti@gmail.com>
2025-09-30 14:55:08 +0200

    ADD: founded the bug. the problem was in initialization. i reinitilize the grid with the original pattern every single time  the function was getting called instead of only in first call

--- life_simulate.c
+++ life_simulate.c
@@ -22,22 +22,37 @@ uint8_t *simulate_life(uint32_t grid_dim, start_coord_t *initial_points, uint32_
     1. Allocate a grid of size grid_dim x grid_dim using VirtualAlloc.
   */
  static uint8_t *grid = NULL;
+ static int first_call = 1;
 
   if (!grid)
   {
     grid = (uint8_t *)VirtualAlloc(NULL, grid_dim * grid_dim, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
     //printf("Failed to allocate memory for the grid.\n");
-  }  
-  for (uint32_t i = 0; i < initial_point_count; i++)
-  {
-    start_coord_t c = initial_points[i];
-    grid[c.y * grid_dim + c.x] = 1;
+    for (uint32_t i = 0; i < grid_dim * grid_dim; i++)
+    {
+      grid[i] = 0;
+    }
   }
+  if(first_call)
+  {
+    for (uint32_t i = 0; i < initial_point_count; i++)
+    {
+      start_coord_t c = initial_points[i];
+      grid[c.y * grid_dim + c.x] = 1;
+    }
+    first_call = 0;
+    return grid;  
+  }  
   uint8_t *new_grid = (uint8_t *)VirtualAlloc(NULL, grid_dim * grid_dim, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
   if (!new_grid)
   {
     return NULL;
   }
+  for (uint32_t i = 0; i < grid_dim * grid_dim; i++)
+  {
+    new_grid[i] = 0;
+  }
+  
   for (uint32_t y = 0; y < grid_dim; y++)
   {
     for (uint32_t x = 0; x < grid_dim; x++)
@@ -50,8 +65,14 @@ uint8_t *simulate_life(uint32_t grid_dim, start_coord_t *initial_points, uint32_
         {
           if (dx == 0 && dy == 0)
             continue; 
-          uint32_t nx = (x + dx + grid_dim) % grid_dim;
-          uint32_t ny = (y + dy + grid_dim) % grid_dim;
+          // uint32_t nx = (x + dx + grid_dim) % grid_dim;
+          // uint32_t ny = (y + dy + grid_dim) % grid_dim;
+          int32_t temp_x = (int32_t)x + dx;
+          int32_t temp_y = (int32_t)y + dy;
+          
+          uint32_t nx = ((temp_x % (int32_t)grid_dim) + grid_dim) % grid_dim;
+          uint32_t ny = ((temp_y % (int32_t)grid_dim) + grid_dim) % grid_dim;
+         
           live_neighbors += grid[ny * grid_dim + nx];
         }
       }
